9af4c342d2874781da62609611265fd4d8bf84e4
王富昕 <i@ego1st.cn>
2021-07-23 18:01:59 +0800

    add footer time

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,41 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
+import { showDateTime } from "../../js/since";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 function Footer(props) {
-  function show_date_time () {
-    window.setTimeout(function () {
-      show_date_time();
+  const { siteConfig } = useDocusaurusContext();
+  const { customFields } = siteConfig;
+  const { footer } = customFields;
+  const { since } = footer;
+
+  const [count, setCount] = useState(showDateTime(since));
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount(showDateTime(since));
     }, 1000);
-    var BirthDay = new Date(window.CONFIG.since);
-    var today = new Date();
-    var timeold = (today.getTime() - BirthDay.getTime());
-    var msPerDay = 24 * 60 * 60 * 1000;
-    var e_daysold = timeold / msPerDay;
-    var daysold = Math.floor(e_daysold);
-    var e_hrsold = (e_daysold - daysold) * 24;
-    var hrsold = Math.floor(e_hrsold);
-    var e_minsold = (e_hrsold - hrsold) * 60;
-    var minsold = Math.floor((e_hrsold - hrsold) * 60);
-    var seconds = Math.floor((e_minsold - minsold) * 60);
-    $('#since').html(daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒");
-  }
-
-
-
-
-
-
-
-
+    return () => clearInterval(id);
+  }, []);
 
   return (
     <footer className="footer">
       <div className="footer-image"></div>
       <div className="footer-inner"></div>
       <p>
-        博客已萌萌哒运行<span id="since"></span>
-        <span class="my-face">(●'◡'●)ﾉ♥</span>
+        博客已萌萌哒运行<span id="since">{count}</span>
+        <span className="my-face">(●'◡'●)ﾉ♥</span>
       </p>
-
     </footer>
   );
 }

613ebe2e0ac7d0ab460dff7f2870c591a2ef7f45
EGo14T <974412906@qq.com>
2021-09-20 12:23:45 +0800

    tag

--- src/theme/BlogTagsPostsPage/index.jsx
+++ src/theme/BlogTagsPostsPage/index.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import Header from "../Header";
+import Navbar from '@theme/Navbar';
+import Head from "@docusaurus/Head";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Main from "../Main";
+
+
+
+import 'aplayer/dist/APlayer.min.css';
+import APlayer from 'aplayer';
 
 function BlogTagsPostPage(props) {
+
+    const { siteConfig } = useDocusaurusContext();
+    const { customFields } = siteConfig;
+    const { header_bg } = customFields;
     return (
         <Layout>
-            <Header />
+            <header className="header">
+                <div className="header-inner">
+                    <div className="site-brand-wrapper" style={{ backgroundImage: `url(${header_bg})` }}>
+                        <Navbar />
+                        <Head>
+                            <meta name="keywords" content="ego1st.cn" />
+                            <title>ego1st.cn</title>
+                        </Head>
+                    </div>
+                </div>
+            </header>
+            <Main>
+                <div style={{width:"100%", height:"100%", backgroundColor:"red"}}></div>
+            </Main>
         </Layout>
     );
 }

cd0cfadb5dabd521f6d8d0993c2612a6e6376d8c
EGo14T <974412906@qq.com>
2021-10-22 23:10:01 +0800

    1

--- src/components/aplayer/index.jsx
+++ src/components/aplayer/index.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useRef } from "react";
-import "APlayer/dist/APlayer.min.css";
-import APlayer from "APlayer";
-import axios from "axios";
+// import React, { useEffect, useRef } from "react";
+// import "APlayer/dist/APlayer.min.css";
+// import APlayer from "APlayer";
+// import axios from "axios";
 
-function EAplayer(props) {
-    const api = "https://api.i-meto.com/meting/api?server=netease&type=song&id=1383893826";
-    const { songId } = props;
-    const aplayerElm = useRef();
+// function EAplayer(props) {
+//     const api = "https://api.i-meto.com/meting/api?server=netease&type=song&id=1383893826";
+//     const { songId } = props;
+//     const aplayerElm = useRef();
 
-    useEffect(() => {
-        axios.get(api).then((res) => {
-            new APlayer({
-                showlrc: 3,
-                theme: "#8d7561",
-                music: res.data,
-                container: aplayerElm.current,
-            });
-        });
-    }, []);
+//     useEffect(() => {
+//         axios.get(api).then((res) => {
+//             new APlayer({
+//                 showlrc: 3,
+//                 theme: "#8d7561",
+//                 music: res.data,
+//                 container: aplayerElm.current,
+//             });
+//         });
+//     }, []);
 
-    return <div ref={aplayerElm} />;
-}
+//     return <div ref={aplayerElm} />;
+// }
 
-export default EAplayer;
+// export default EAplayer;

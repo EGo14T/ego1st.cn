abd2aa3aca3cf137a6de29a03b28e52b2086a9da
EGo14T <974412906@qq.com>
2022-01-08 18:04:37 +0800

    add dark theme

--- src/theme/BlogSidebar/index.jsx
+++ src/theme/BlogSidebar/index.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import TOCHeadings from "@theme/TOC";
-import clsx from "clsx";
+import React, { useEffect, useState } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import TOCHeadings from '@theme/TOC';
+import clsx from 'clsx';
+import UpYunSvg from '../../../static/img/upyun.svg';
 
 function BlogSidebar(props) {
     const { siteConfig } = useDocusaurusContext();
@@ -18,27 +19,34 @@ function BlogSidebar(props) {
             <div className="sidebar-inner">
                 {isBlogPostPage && (
                     <ul className="sidebar-nav">
-                        <li className={clsx(tocType === 1 && "sidebar-nav-active")} onClick={() => setTocType(1)}>
+                        <li className={clsx(tocType === 1 && 'sidebar-nav-active')} onClick={() => setTocType(1)}>
                             文章目录
                         </li>
-                        <li className={clsx(tocType === 2 && "sidebar-nav-active")} onClick={() => setTocType(2)}>
+                        <li className={clsx(tocType === 2 && 'sidebar-nav-active')} onClick={() => setTocType(2)}>
                             关于我
                         </li>
                     </ul>
                 )}
                 {isBlogPostPage && (
-                    <section className={clsx(tocType === 2 && "section-hide", tocType === 1 && "section-show")}>
+                    <section className={clsx(tocType === 2 && 'section-hide', tocType === 1 && 'section-show')}>
                         <TOCHeadings toc={toc} />
                     </section>
                 )}
-                <section className={clsx(tocType === 1 && "section-hide", tocType === 2 && "section-show")}>
+                <section className={clsx(tocType === 1 && 'section-hide', tocType === 2 && 'section-show')}>
                     <p className="site-description motion-element" itemProp="description">
                         联系我
                     </p>
                     <div className="links-of-author">
                         {social.map((item) => (
                             <span className="links-of-author-item" key={item.title}>
-                                <a href={item.url} target="_blank" title={item.title} data-balloon={item.title} data-balloon-pos="up" dangerouslySetInnerHTML={{ __html: item.icon }} />
+                                <a
+                                    href={item.url}
+                                    target="_blank"
+                                    title={item.title}
+                                    data-balloon={item.title}
+                                    data-balloon-pos="up"
+                                    dangerouslySetInnerHTML={{ __html: item.icon }}
+                                />
                             </span>
                         ))}
                     </div>
@@ -47,7 +55,7 @@ function BlogSidebar(props) {
                     </p>
                     <div className="cc-license motion-element" itemProp="license">
                         <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" className="cc-opacity" target="_blank">
-                            <img src="https://www.laobuluo.com/banner/upyun.png" alt="Creative Commons" />
+                            <UpYunSvg/>
                         </a>
                     </div>
                     <div className="feed-link">
